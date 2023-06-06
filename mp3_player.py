631a24c57d10202aa2e77621211c66ff5cc448d7
amora324 <amora324@fiu.edu>
2023-06-05 20:05:48 -0400

    progress

--- mp3_player.py
+++ mp3_player.py
@@ -1,4 +1,6 @@
 from tkinter import *
+
+#import pygame.mixer.music
 from PIL import ImageTk, Image
 from tkinter import filedialog, ttk
 from pygame import mixer
@@ -8,7 +10,7 @@ from mutagen.mp3 import MP3
 mixer.init()
 root = Tk()
 root.title('MP3 Player')
-root.geometry("420x280")
+root.geometry("420x290")
 
 playlist = []
 
@@ -33,48 +35,62 @@ def convert_time(time):
         return str(minutes) + ":" + str(seconds)
 
 def add_song():
+    mixer.music.pause()
     filename = filedialog.askopenfilename(initialdir="/", title="Select A File", filetypes=(("mp3 files", "*.mp3"), ("all files", "*.*")))
-    songname = song_rename(filename)
-    songinfo = (songname, filename)
-    playlist.append(songinfo)
-    lastindex = len(playlist)-1
-    listbox.insert(END, playlist[lastindex][0])
+    if filename != '':
+        songname = song_rename(filename)
+        songinfo = (songname, filename)
+        playlist.append(songinfo)
+        lastindex = len(playlist)-1
+        listbox.insert(END, playlist[lastindex][0])
+    mixer.music.unpause()
 
 def remove_song():
     current = listbox.curselection()
     current = current[0]
+    if current_song == playlist[current][1]:
+        stop()
     listbox.delete(current)
     playlist.remove((playlist[current][0], playlist[current][1]))
 
 def get_time():
     total_seconds = int(mixer.music.get_pos()/1000)
-    song = playlist[(listbox.curselection())[0]][1]
-    song_mut = MP3(song)
+    song_mut = MP3(current_song)
     song_length = int(song_mut.info.length)
     if paused:
         pass
     else:
-        if song_length == total_seconds:
+        if song_length == int(song_slider.get()):
             song_slider.config(to=song_length, value=song_length)
             elapsed_time.config(text=convert_time(song_length))
-        elif song_slider.get() == total_seconds:
-            song_slider.config(to=song_length, value=total_seconds)
-            elapsed_time.config(text=convert_time(total_seconds))
+        #elif song_slider.get() != total_seconds:
+            #song_slider.config(to=song_length, value=total_seconds)
+            #elapsed_time.config(text=convert_time(total_seconds))
+            #return
         else:
             song_slider.config(to=song_length,value=int(song_slider.get()))
             elapsed_time.config(text=convert_time(song_slider.get()))
             next_time = int(song_slider.get()) + 1
             song_slider.config(to=song_length, value=next_time)
-        elapsed_time.after(1000, get_time)
+    elapsed_time.after(1000, get_time)
     total_time.config(text=convert_time(song_length))
 
+def stop():
+    mixer.music.stop()
+    song_slider.config(value=0)
+    elapsed_time.config(text="0:00")
+
+current_song = ''
+
 def play():
+    global current_song
     song_slider.config(value=0)
     elapsed_time.config(text="0:00")
     current = listbox.curselection()
     current = current[0]
+    current_song = playlist[current][1]
 
-    mixer.music.load(playlist[current][1])
+    mixer.music.load(current_song)
     mixer.music.play()
 
     label.configure(text="Now Playing\n" + listbox.get(listbox.curselection()))
@@ -95,7 +111,7 @@ def pause():
 def back():
     song_slider.config(value=0)
     elapsed_time.config(text="0:00")
-    index = listbox.curselection()
+    index = listbox.curselection() #changed
     index = index[0]
     if index == 0:
         listbox.selection_clear(0, END)
@@ -104,7 +120,7 @@ def back():
         mixer.music.play()
         label.configure(text="Now Playing\n" + listbox.get(listbox.size()-1))
     else:
-        prev_song = listbox.curselection()
+        prev_song = listbox.curselection() #changed
         prev_song = prev_song[0] - 1
         mixer.music.load(playlist[prev_song][1])
         mixer.music.play()
@@ -115,7 +131,7 @@ def back():
 def next():
     song_slider.config(value=0)
     elapsed_time.config(text="0:00")
-    index = listbox.curselection()
+    index = listbox.curselection() #changed
     index = index[0]
     if index == listbox.size() - 1:
         listbox.selection_clear(0, END)
@@ -124,7 +140,7 @@ def next():
         mixer.music.play()
         label.configure(text="Now Playing\n" + listbox.get(0))
     else:
-        next_song= listbox.curselection()
+        next_song= listbox.curselection() #changed
         next_song = next_song[0] + 1
         mixer.music.load(playlist[next_song][1])
         mixer.music.play()
@@ -139,14 +155,15 @@ def scrub(time):
     mixer.music.load(playlist[current][1])
     mixer.music.play(start=song_slider.get())
 
-def volume():
-    return
+def volume(vol):
+    mixer.music.set_volume(volume_slider.get())
 
 #images
 img1 = PhotoImage(file='images/back.png')
 img2 = PhotoImage(file='images/next.png')
 img3 = PhotoImage(file='images/pause.png')
 img4 = PhotoImage(file='images/play.png')
+img5 = PhotoImage(file='images/volume.png')
 
 btn = Button(root, text="Add Song", command=add_song).grid(row=1,column=0)
 remove_btn = Button(root, text="Remove Song", command=remove_song).grid(row=2, column=0)
@@ -156,6 +173,9 @@ pause_btn = Button(root, image=img3, command=pause).grid(row=1,column=4, sticky=
 next_btn = Button(root, image=img2, command=next).grid(row=1,column=5, sticky=E)
 
 blanklabel = Label(root).grid(row=1,column=1)
+
+volume_frame = LabelFrame(root, bd=0)
+volume_frame.grid(row=0, column=6, columnspan=5)
 
 label = Label(root, text="Now Playing",font=("Courier", 15), wraplength=180)
 label.grid(row=0,column=2, columnspan=5)
@@ -169,7 +189,8 @@ total_time.grid(row=3,column=5)
 song_slider = ttk.Scale(root, from_=0, to=1, orient=HORIZONTAL, value=0, command=scrub, length=200)
 song_slider.grid(row=2, column=2, columnspan=5)
 
-volume_slider = ttk.Scale(root, from_=0, to=1, orient=VERTICAL, value=1, command=volume, length=150)
-volume_slider.grid(row=0, column=6, columnspan=5)
+volume_label = Label(volume_frame, image=img5).pack()
+volume_slider = ttk.Scale(volume_frame, from_=0, to=1, orient=VERTICAL, value=1, command=volume, length=160)
+volume_slider.pack()
 
 root.mainloop()
